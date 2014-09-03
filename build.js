efb6727396814e1929aa584dca952eb6fa4e5bb6
dubrowgn <dustin.brown@dubrowgn.com>
2014-09-03 10:06:30 -0600

    Fixed requirejs build issue

--- build.js
+++ build.js
@@ -1,8 +1,19 @@
 // this is a requirejs build config file
 
 ({
-    baseUrl: "src",
-    name: "main",
-    out: "dist/main.js",
-    removeCombined: true
+	shim: {
+		'../lib/jquery-2.1.1': {
+			exports: '$'
+		},
+		'../lib/keymaster': {
+			exports: 'key'
+		},
+		'../lib/underscore': {
+			exports: '_'
+		}
+	},
+	baseUrl: "src",
+	name: "main",
+	out: "dist/main.js",
+	removeCombined: true
 })
